076b691c11ffa684a6df5aafdaeebbdbd9253442
Gustavo Medeiros <eugus03@gmail.com>
2022-12-30 22:26:29 -0300

    advancing

---
+++ curso-02/distinct.sql
@@ -0,0 +1,3 @@
+-- Query para retornar dados únicos de dados iguais na tabela de produtos.
+
+SELECT DISTINCT EMBALAGEM, TAMANHO FROM tabela_de_produtos WHERE SABOR = 'Laranja';
---
+++ curso-02/order-by.sql
@@ -0,0 +1,4 @@
+-- Query ordenada de forma ASCENDENTE E DESCENDENTE.
+
+SELECT * FROM tabela_de_produtos ORDER BY PRECO_DE_LISTA ASC;
+SELECT * FROM tabela_de_produtos ORDER BY PRECO_DE_LISTA DESC;
