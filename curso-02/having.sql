3255a0456bf4464f1499b47d33e1c5c001f201ad
Gustavo Medeiros <eugus03@gmail.com>
2023-01-04 20:36:56 -0300

    advancing

---
+++ curso-02/having.sql
@@ -0,0 +1,3 @@
+-- Query de estado que possui limite de credito maior que 900mil.
+SELECT ESTADO, SUM(LIMITE_DE_CREDITO) AS SOMA_LIMITE FROM tabela_de_clientes
+GROUP BY ESTADO HAVING SUM(LIMITE_DE_CREDITO) > 900000;
