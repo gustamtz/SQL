296c8bce9bd94b5097f2f8101b61f4b1215a6a0a
Gustavo Medeiros <eugus03@gmail.com>
2023-01-05 12:17:25 -0300

    advancing

--- curso-02/join.sql
+++ curso-02/join.sql
@@ -8,3 +8,12 @@ INNER JOIN notas_fiscais B
 ON A.MATRICULA = B.MATRICULA 
 GROUP BY A.MATRICULA, A.NOME;
 
+-- Left Join
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_vendedores A
+LEFT JOIN notas_fiscais B ON A.CPF = B.CPF
+WHERE B.CPF IS NULL;
+
+-- Righr Join
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_vendedores B
+RIGHT JOIN notas_fiscais A ON A.CPF = B.CPF
+WHERE B.CPF IS NULL;
